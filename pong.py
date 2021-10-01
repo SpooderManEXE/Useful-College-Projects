06e0c0b46d12f07abbdd6e4597edc146d98abb6e
TheSpeedX <ggspeedx29@gmail.com>
2021-10-01 15:21:37 +0530

    formatted code

--- pong.py
+++ pong.py
@@ -23,6 +23,7 @@ PLAYER_MOVEMENT_UNITS = 5
 COMPUTER_MOVEMENT_UNITS = 6
 BALL_DEFAULT_SPEED = 3
 
+
 class Ball:
     def __init__(self):
         self.x = 0
@@ -42,7 +43,7 @@ class Ball:
     def throw_in(self):
         self.x = SCREEN_WIDTH // 2
         self.y = SCREEN_HEIGHT // 2
-        self.direction_x = 1 if random.randint(0,1) % 2 else -1
+        self.direction_x = 1 if random.randint(0, 1) % 2 else -1
         self.speed = BALL_DEFAULT_SPEED
 
     def move(self):
@@ -56,8 +57,9 @@ class Ball:
     def leaves_screen(self):
         return self.x + self.radius < 0 or self.x - self.radius > SCREEN_WIDTH
 
+
 class Paddle:
-    def __init__(self, x, color = WHITE):
+    def __init__(self, x, color=WHITE):
         self.x = x
         self.y = SCREEN_HEIGHT // 2
         self.width = PADDLE_WIDTH
@@ -68,11 +70,13 @@ class Paddle:
         self.predicted_y = None
 
     def collides_with_ball(self, ball):
-        left_side_hit, left_side_overlap = self.__collides_with_ball(ball, self.x)
+        left_side_hit, left_side_overlap = self.__collides_with_ball(
+            ball, self.x)
         if left_side_hit:
             return left_side_hit, left_side_overlap
 
-        right_side_hit, right_side_overlap = self.__collides_with_ball(ball, self.x + self.width)
+        right_side_hit, right_side_overlap = self.__collides_with_ball(
+            ball, self.x + self.width)
         return right_side_hit, right_side_overlap
 
     def __collides_with_ball(self, ball, x):
@@ -94,7 +98,7 @@ class Paddle:
             y0 = (-b + (sqrt_discriminant)) / (2 * a)
             y1 = (-b - (sqrt_discriminant)) / (2 * a)
             return (True, self.y < y0 < self.y + self.height or self.y < y1 < self.y + self.height)
-        
+
 
 def computer_predict(ball, computer):
     predicted_dir = ball.direction_y
@@ -105,16 +109,20 @@ def computer_predict(ball, computer):
         y += ball.speed * predicted_dir
     return y
 
+
 def ai_move(ball, computer):
     if not round_winner and ball.direction_x == 1 and ball.x > SCREEN_WIDTH // 2:
         predicted_y = computer.predicted_y or computer_predict(ball, computer)
         if predicted_y > 0 and predicted_y < SCREEN_HEIGHT and (predicted_y < computer.y or predicted_y > computer.y + computer.height):
-            computer.y += COMPUTER_MOVEMENT_UNITS * (-1 if predicted_y < computer.y else 1)
+            computer.y += COMPUTER_MOVEMENT_UNITS * \
+                (-1 if predicted_y < computer.y else 1)
+
 
 def display_text(text, x, y):
     myfont = pygame.font.SysFont('Comic Sans MS', 30)
     textsurface = myfont.render(text, False, WHITE)
-    screen.blit(textsurface, (x, y))    
+    screen.blit(textsurface, (x, y))
+
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -123,8 +131,8 @@ pygame.font.init()
 
 ball = Ball()
 ball.throw_in()
-player = Paddle(x = 30, color = LIGHT_BLUE)
-computer = Paddle(x = SCREEN_WIDTH - 60, color = BROWN)
+player = Paddle(x=30, color=LIGHT_BLUE)
+computer = Paddle(x=SCREEN_WIDTH - 60, color=BROWN)
 round_winner = None
 tick = 0
 last_key = 1
@@ -141,10 +149,10 @@ while True:
         ball.throw_in()
         pygame.time.delay(ROUND_WIN_DELAY)
         continue
-    
+
     player_goal_reached, player_defended = player.collides_with_ball(ball)
     # The ball crosses the goal line
-    if player_goal_reached:       
+    if player_goal_reached:
         # The round hasn't been won yet, the player hits the ball back
         if not round_winner and player_defended:
             ball.flip_direction_x()
@@ -156,8 +164,9 @@ while True:
         # The round has been won, though the paddle hits the ball
         elif round_winner and player_defended:
             ball.flip_direction_y()
-        
-    computer_goal_reached, computer_defended = computer.collides_with_ball(ball)
+
+    computer_goal_reached, computer_defended = computer.collides_with_ball(
+        ball)
     if computer_goal_reached:
         if not round_winner and computer_defended:
             ball.flip_direction_x()
@@ -165,7 +174,7 @@ while True:
             round_winner = player
         elif round_winner and computer_defended:
             ball.flip_direction_y()
-            
+
     # Speed up the game by one unit in every 1000 ticks
     tick += 1
     if tick % 1000 == 0:
@@ -178,12 +187,15 @@ while True:
     display_text(str(player.score), 180, 45)
     display_text('Computer', 400, 0)
     display_text(str(computer.score), 460, 45)
-    pygame.draw.rect(screen, player.color, (player.x, player.y, player.width, player.height), 0)
-    pygame.draw.rect(screen, computer.color, (computer.x, computer.y, computer.width, computer.height), 0)
+    pygame.draw.rect(screen, player.color, (player.x,
+                                            player.y, player.width, player.height), 0)
+    pygame.draw.rect(screen, computer.color, (computer.x,
+                                              computer.y, computer.width, computer.height), 0)
     pygame.draw.circle(screen, ball.color, (ball.x, ball.y), ball.radius)
     for i in range(1, SCREEN_HEIGHT // 10):
         if i % 2 == 0:
-            pygame.draw.rect(screen, GRAY, (SCREEN_WIDTH // 2, i * 10, 10, 10), 0)
+            pygame.draw.rect(
+                screen, GRAY, (SCREEN_WIDTH // 2, i * 10, 10, 10), 0)
     pygame.display.update()
 
     # Event handling
@@ -193,16 +205,18 @@ while True:
         goal_reached, overlap = player.collides_with_ball(ball)
         if goal_reached and player.y - PLAYER_MOVEMENT_UNITS < ball.y + ball.radius:
             continue
-        player.y = 0 if player.y - PLAYER_MOVEMENT_UNITS < 0 else player.y - PLAYER_MOVEMENT_UNITS 
+        player.y = 0 if player.y - \
+            PLAYER_MOVEMENT_UNITS < 0 else player.y - PLAYER_MOVEMENT_UNITS
         last_key = -1
-        
+
     if keys[pygame.K_DOWN]:
         goal_reached, overlap = player.collides_with_ball(ball)
         if goal_reached and player.y + player.height + PLAYER_MOVEMENT_UNITS > ball.y - ball.radius:
             continue
-        player.y = SCREEN_HEIGHT - player.height if player.y + player.height + PLAYER_MOVEMENT_UNITS > SCREEN_HEIGHT else player.y + PLAYER_MOVEMENT_UNITS
+        player.y = SCREEN_HEIGHT - player.height if player.y + player.height + \
+            PLAYER_MOVEMENT_UNITS > SCREEN_HEIGHT else player.y + PLAYER_MOVEMENT_UNITS
         last_key = 1
-        
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
