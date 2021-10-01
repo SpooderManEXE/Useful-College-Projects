b019cb27fa7fd11d2aa2d16cc82a138a5c6255f8
Sooraj S Nair <nairsooraj2000@gmail.com>
2021-10-01 15:08:59 +0530

    Add files via upload

---
+++ url-checker.py
@@ -0,0 +1,9 @@
+import re
+  
+def Find(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)      
+    return [x[0] for x in url]
+
+string = 'My Profile: https://auth.geeksforgeeks.org/user/Chinmoy%20Lenka/articles in the portal of http://www.geeksforgeeks.org/'
+print("Urls: ", Find(string))
